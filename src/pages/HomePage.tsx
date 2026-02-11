edbf2a9ba28473099a110e869d5e404420c71373
Finn Kitzmann <finnkitzmann@Finns-MacBook-Air.local>
2026-02-11 13:50:54 +0100

    ...

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Splash from "../components/Splash";
 import Hero from "../components/Hero";
 import Events from "../components/Events";
 import Intro from "../components/Intro";
@@ -9,30 +8,8 @@ import Reviews from "../components/Reviews";
 import Info from "../components/Info";
 
 export default function HomePage() {
-  const [showSplash, setShowSplash] = React.useState(() => {
-    if (typeof window === "undefined") return false;
-    try {
-      // Show once per browser session (so it doesn't disappear forever).
-      return !window.sessionStorage.getItem("keppler_splash_seen_v1");
-    } catch {
-      return true;
-    }
-  });
-
   return (
     <>
-      {showSplash ? (
-        <Splash
-          onDone={() => {
-            try {
-              window.sessionStorage.setItem("keppler_splash_seen_v1", "1");
-            } catch {
-              // ignore
-            }
-            setShowSplash(false);
-          }}
-        />
-      ) : null}
       <Hero />
       <Events />
       <Intro />
